1b9706be9a252e72ad15eec63da604aa0e9ac995
Gostuski <dusangp@gmail.com>
2019-12-25 00:05:05 +0100

    Registration and homepage with blog posts
    Users app with registration and validation.
    Display of all posted blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import Post
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'title': 'Blog_board',
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return HttpResponse(render(request, 'blog/about.html'))
