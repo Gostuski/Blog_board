9fd6951fd2063854737d7b0dd3dee1d14200a0d2
Gostuski <dusangp@gmail.com>
2019-12-25 16:59:21 +0100

    Log in and profile
    Login system
    Profile creation and updating

--- blog_board/settings.py
+++ blog_board/settings.py
@@ -116,10 +116,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
